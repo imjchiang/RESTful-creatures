68c5617fbcc463bdc574e110ef928db160570034
imjchiang <jchiang296@gmail.com>
2020-08-06 14:21:59 -0700

    [JS] add new prehis route [EJS] fix show image, add new form

--- controllers/prehistoric_creatures.js
+++ controllers/prehistoric_creatures.js
@@ -10,4 +10,19 @@ router.get("/", function(req, res)
     res.render("prehistoric-creatures/index", {myPrehis: prehisData});
 })
 
+router.get("/new", function(req, res)
+{
+    res.render("prehistoric-creatures/new");
+})
+
+router.get("/:id", function(req, res)
+{
+    let prehistorics = fs.readFileSync("././prehistoric_creatures.json");
+    let prehisData = JSON.parse(prehistorics);
+
+    let prehisIndex = req.params.id;
+    
+    res.render("prehistoric-creatures/show", {myPrehis: prehisData[prehisIndex]});
+})
+
 module.exports = router;
